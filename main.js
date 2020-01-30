bdf96905c6516191d32f4675dc021797902e81d7
s0ick <36438226+s0ick@users.noreply.github.com>
2020-01-30 17:13:15 +0300

    bot update | js

--- main.js
+++ main.js
@@ -1,30 +1,48 @@
 'use strict';
 
 let num = prompt('Угадай число от 1 до 100:');
+let count = 10;
 
 function getInput(val){
-  return function (num) {
+  return function (count, num) {
     if(num === null){
       alert('Игра выключена');
       return 'Пользователь нажал "Отмена"';
+    } else if(count === 0) {
+      if(confirm('Попытки закончились, хотите сыграть еще?')){
+        count = 10;
+        num = prompt('Угадай число от 1 до 100:');
+        return conclusion(count, num);
+      } else {
+        alert('Игра выключена');
+        return 'Пользователь нажал "Отмена"';
+      }
     } else if(isNaN(num) || num === '') {
       alert('Введите число!');
       num = prompt('Угадай число от 1 до 100:');
-      return conclusion(num);
+      return conclusion(count, num);
     } else if(+num < val) {
-      alert('Загаданное число больше');
+      count--;
+      alert(`Загаданное число больше, осталось попыток ${count}`);
       num = prompt('Угадай число от 1 до 100:');
-      return conclusion(num);
+      return conclusion(count, num);
     } else if(+num > val) {
-      alert('Загаданное число меньше');
+      count--;
+      alert(`Загаданное число меньше, осталось попыток ${count}`);
       num = prompt('Угадай число от 1 до 100:');
-      return conclusion(num);
+      return conclusion(count, num);
     } else if(+num === val) {
-      alert('Поздравляю, игра пройдена!');
-      return 'Это было число 47!';
+      if(confirm('Поздравляю, игра пройдена! Хотите сыграть еще?')){
+        count = 10;
+        num = prompt('Угадай число от 1 до 100:');
+        return conclusion(count, num);
+      } else {
+        alert('Игра выключена');
+        return 'Это было число 47!';
+      }
     } 
   }
 }
 const conclusion = getInput(47);
 console.dir(conclusion);
-console.log(conclusion(num));
+console.log(conclusion(count, num));
