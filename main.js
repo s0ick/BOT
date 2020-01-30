1c0cd7b2356e1106ddb8e9508df7fa96e72d2707
s0ick <36438226+s0ick@users.noreply.github.com>
2020-01-30 16:53:46 +0300

    feature: add closure var | js

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
 'use strict';
 
-function getInput(val){
-  let num = prompt('Угадай число от 1 до 100:');
+let num = prompt('Угадай число от 1 до 100:');
 
-  function checkVal(v, num) {
+function getInput(val){
+
+  return function (num) {
     if(num === null){
       alert('Игра выключена');
       return 'Пользователь нажал "Отмена"';
     } else if(isNaN(num) || num === '') {
       alert('Введите число!');
       num = prompt('Угадай число от 1 до 100:');
-      return checkVal(v, num);
-    } else if(+num < v) {
+      return conclusion(num);
+    } else if(+num < val) {
       alert('Загаданное число больше');
       num = prompt('Угадай число от 1 до 100:');
-      return checkVal(v, num);
-    } else if(+num > v) {
+      return conclusion(num);
+    } else if(+num > val) {
       alert('Загаданное число меньше');
       num = prompt('Угадай число от 1 до 100:');
-      return checkVal(v, num);
-    } else if(+num === v) {
+      return conclusion(num);
+    } else if(+num === val) {
       alert('Поздравляю, игра пройдена!');
       return 'Это было число 47!';
     } 
   }
-  return checkVal(val, num);
 }
-console.log(getInput(47));
+const conclusion = getInput(47);
+console.dir(conclusion);
+console.log(conclusion(num));
